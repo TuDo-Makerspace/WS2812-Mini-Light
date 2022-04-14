aeec676617116ce91be94997969fde49095cf12d
CTXz <ctx.xda@gmail.com>
2022-04-14 23:56:46 +0200

    Added README
    Signed-off-by: CTXz <ctx.xda@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2022 Patrick Pedersen, TUDO Makerspace
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * Description: Main code for the WS2812-Mini-Light Controller
+ * 
+ */
+
 #include <ws2812_cpp.h>
 
 #ifdef WS2812_TARGET_PLATFORM_ARDUINO_AVR
